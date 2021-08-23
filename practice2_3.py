996d3ed80e07f58a7ec807927b29b2d2a10062b3
Seagullflysxt <89381353+Seagullflysxt@users.noreply.github.com>
2021-08-23 16:08:46 +0800

    Add files via upload

---
+++ practice2_3.py
@@ -0,0 +1,6 @@
+#practice2_3
+name = "eric"
+message = f"Hello {name.title()},would you like to learn some Python today?"
+#message
+print(message)
+print(f"hello {name.title()},would you like to learn some Python today?")
